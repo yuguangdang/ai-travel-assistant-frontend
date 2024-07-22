c05759cd24421967e49473a0f6117d2cfc065efe
yuguang.dang <yuguang.dang@travelctm.com>
2024-07-23 07:16:30 +1000

    switch to normal HTTP

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ChatRESTSSE from "./ChatRESTSSE";
+import ChatREST from "./ChatREST";
 import "./App.css";
 
 const App = () => {
     return (
         <div className="app-container">
             <div className="chat-components">
-                <ChatRESTSSE/>
+                <ChatREST/>
             </div>
         </div>
     );
